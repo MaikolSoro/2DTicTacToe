3df4f9b3aacc570f0d2da56fb06cfb2178be83c8
Maikol Soro <maikolsoro.z1998@gmail.com>
2020-06-13 13:37:54 -0600

    Modelo de jugadas

--- app/src/main/java/com/example/tictactoe/ui/FindGameActivity.java
+++ app/src/main/java/com/example/tictactoe/ui/FindGameActivity.java
@@ -3,14 +3,21 @@
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.example.tictactoe.R;
 
     public class FindGameActivity extends AppCompatActivity {
-
+    private TextView tvLoadingMessage;
+    private ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_game);
+        tvLoadingMessage = findViewById(R.id.textViewLoading);
+        progressBar = findViewById(R.id.progressBarJugadas);
+        progressBar.setIndeterminate(true);
+        tvLoadingMessage.setText("Cargando...");
     }
 }
